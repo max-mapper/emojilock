1e5847c8d3ccb71dcf7f1923f95a6ec42e37af41
Max Ogden <max@maxogden.com>
2015-07-29 17:22:11 -0700

    add decode

--- cli.js
+++ cli.js
@@ -5,18 +5,25 @@ var path = require('path')
 var emojilock = require('./')
 var id = process.argv[2]
 
-if (id) {
+if (!id) {
+  tryProfile()
+} else if (id === 'decode') {
+  console.log(emojilock.decode(process.argv[3]))
+  process.exit(0)
+} else {
   console.log(emojilock.encode(id))
   process.exit(0)
 }
 
-var minilockPath = path.join(process.env.HOME || process.env.USERPROFILE, '.mlck', 'profile.json')
-fs.readFile(minilockPath, function (err, buf) {
-  if (err) {
-    console.error('Could not find minilock-cli profile')
-    process.exit(1)
-  }
-  var profile = JSON.parse(buf)
-  console.log(emojilock.encode(profile.id))
-  process.exit(0)
-})
+function tryProfile() {
+  var minilockPath = path.join(process.env.HOME || process.env.USERPROFILE, '.mlck', 'profile.json')
+  fs.readFile(minilockPath, function (err, buf) {
+    if (err) {
+      console.error('Could not find minilock-cli profile')
+      process.exit(1)
+    }
+    var profile = JSON.parse(buf)
+    console.log(emojilock.encode(profile.id))
+    process.exit(0)
+  })
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,31 @@
 # emojilock
 
 converts minilock ids, e.g. 25DAuXM9z84c4iEXazuvwtBX2651pLa4xp5bF2p3pJqKMw into base-emoji representations, e.g. 🍮💡©✈️😖🎪🐘🎪💣📀😥🕓➡️🚅🍻📋⚓️🔄🐄📈🌂💏🛄🚧📲⬆️📷💫⬇️📘💑⚓️
+
+## install
+
+```
+npm i emojilock -g
+```
+
+## usage
+
+first, use `npm i minilock-cli` to create a minilock id
+
+## encode
+
+```
+emojilock <your-minilock-id>
+```
+
+will convert your minilock id to emoji and print it out
+
+if no args are specified, will try to read the file `~/.mlck/profile.json` to get your minilock-cli id
+
+## decode
+
+```
+emojilock <emoji>
+```
+
+will convert emoji back into a minilock id
